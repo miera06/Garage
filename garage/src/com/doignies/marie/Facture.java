c2b4ee810336a3b24b78f8881777858b8339d79c
Xavier-Loison <xavierloison@gmail.com>
2026-03-16 22:51:31 +0100

    Indentation du code Ajout de commentaires Ajout de final dans le constructeur

--- garage/src/com/doignies/marie/Facture.java
+++ garage/src/com/doignies/marie/Facture.java
@@ -3,27 +3,42 @@ package com.doignies.marie;
 import java.time.LocalDate;
 
 public class Facture {
-    //* Variables
-    private Client client;
-    private Voiture voiture;
-    private LocalDate date;
-    private int prixLocation;
+	// /////////////////
+	// *** Variables ***
+	// /////////////////
+	private Client client;
+	private Voiture voiture;
+	private LocalDate date;
+	private int prixLocation;
 
-    //*constructeur
-    public Facture(Client newClient,
-                   Voiture newVoiture,
-                   int newPrixLocation) {
-        this.client = newClient;
-        this.voiture = newVoiture;
-        this.date = LocalDate.now();
-        this.prixLocation = newPrixLocation;
-    }
-    //*Fonctions
-    public void afficheToi(){
-        System.out.println ("\t\t\t Client : " + this.client.getName());
-        System.out.println ("\t\t\t Voiture : " + this.voiture.getMarque());
-        System.out.println ("\t\t\t Date : " + this.date);
-        System.out.println ("\t\t\t Prix: " + this.prixLocation);
-    }
+	// /////////////////////
+	// *** Constructeurs ***
+	// /////////////////////
+	public Facture(final Client newClient, final Voiture newVoiture, final int newPrixLocation) {
+		// On ne met pas newDate
+		// dans la liste des parametres car lors de la Création d'une facture
+		// 1. On met la date du jour à l'instant T
+		this.client = newClient;
+		this.voiture = newVoiture;
+		this.date = LocalDate.now();
+		this.prixLocation = newPrixLocation;
+	}
 
+	// ///////////////
+	// *** Getters ***
+	// ///////////////
+
+	// ///////////////
+	// *** Setters ***
+	// ///////////////
+
+	// /////////////////
+	// *** Fonctions ***
+	// /////////////////
+	public void afficheToi(){
+		System.out.println ("\t\t\t Client : " + this.client.getName());
+		System.out.println ("\t\t\t Voiture : " + this.voiture.getMarque());
+		System.out.println ("\t\t\t Date : " + this.date);
+		System.out.println ("\t\t\t Prix: " + this.prixLocation);
+	}
 }
