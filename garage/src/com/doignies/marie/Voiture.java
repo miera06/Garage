9c5e23cbb6dcaa5847e43970734ef0d3828f08f5
Xavier-Loison <xavierloison@gmail.com>
2026-03-16 22:52:23 +0100

    Indentation du code Ajout de commentaires Ajout de final dans le constructeur

--- garage/src/com/doignies/marie/Voiture.java
+++ garage/src/com/doignies/marie/Voiture.java
@@ -2,101 +2,128 @@
 package com.doignies.marie;
 
 public class Voiture {
-    //*variables
-    private String marque;
-    private String model;
-    private String color;
-    private int tarifParJour;
-    private String etat;
-    private int nbKm;
-    private Client loueur;
+	// /////////////////
+	// *** Variables ***
+	// /////////////////
+	// TODO : ETAPE 5
+	// TODO : Ajouter une private String plaqueImmatriculation
+	private String marque;
+	private String model;
+	private String color;
+	private int tarifParJour;
+	private String etat;
+	// TODO : ETAPE 3
+	// TODO : Supprimer cette variable
+	// TODO : Je ne sais pas a quoi peut servir cette information
+	private int nbKm;
+	private Client loueur;
 
-    //*constructeur
-    public Voiture (String newMarque,
-                    String newModel,
-                    String newColor,
-                    int newtarifParJour) {
-        this.marque = newMarque;
-        this.model= newModel;
-        this.color = newColor;
-        this.tarifParJour = newtarifParJour;
-        this.etat = "neuf";
-        this.nbKm = 0;
-       this.loueur=null;
-    }
+	// /////////////////////
+	// *** Constructeurs ***
+	// /////////////////////
+	public Voiture(final String newMarque, final String newModel, final String newColor, final int newtarifParJour) {
+		// TODO : ETAPE 5
+		// TODO : decommente la ligne en dessous
+		// TODO : this.plaqueImmatriculation = PlaqueGenerator.genererPlaque();
+		this.marque = newMarque;
+		this.model = newModel;
+		this.color = newColor;
+		this.tarifParJour = newtarifParJour;
+		this.etat = "neuf";
+		this.nbKm = 0;
+		this.loueur = null;
+	}
 
-    public Voiture () {
-    }
-    //* getters et setters
-    public String getMarque() {
-        return marque;
-    }
+	// Le constructeur vide est nécessaire car on en a besoin dans
+	// Garage.renvoiLaVoitureQuiALePlusDeKm
+	// TODO : A VOIR ENSEMBLE
+	public Voiture () {
 
-    public String getModel() {
-        return model;
-    }
+	}
 
+	// ///////////////
+	// *** Getters ***
+	// ///////////////
+	public String getMarque() {
+		return marque;
+	}
 
-    public String getColor() { return this.color;}
-    public int getNbKm() { return this.nbKm;}
+	public String getModel() {
+		return model;
+	}
 
-    // * fonctions
+	public String getColor() {
+		return this.color;
+	}
 
-    public void afficheToi() {
-        System.out.println ("\t" + "- model : " + this.model + " / marque : " + this.marque);
-        if (this.loueur != null) {
-            System.out.println("\t\t" + "- loué par: " + this.loueur.getName());
-        }
-        else {
-            System.out.println ("\t\t" + "- n'est pas louée " );
-            }
+	public int getNbKm() {
+		return this.nbKm;
+	}
 
-    }
+	// ///////////////
+	// *** Setters ***
+	// ///////////////
 
-    public void locationVoiture (Client clientLoueur) {
-        if(this.estLoue()==false){
-            this.loueur=clientLoueur;
-            this.etat="utilisé";
-            clientLoueur.setaDejaEmprunte(true);
-        }
-        else{
-            System.out.println("erreur, voiture déjà louée");
-        }
+	// /////////////////
+	// *** Fonctions ***
+	// /////////////////
+	public void afficheToi() {
+		System.out.println ("\t" + "- model : " + this.model + " / marque : " + this.marque);
+		if (this.loueur != null) {
+			System.out.println("\t\t" + "- loué par: " + this.loueur.getName());
+		} else {
+			System.out.println ("\t\t" + "- n'est pas louée " );
+		}
+	}
 
-    }
+	public void locationVoiture(Client clientLoueur) {
+		// if(this.estLoue()==false) {
+		if(!this.estLoue()) {
+			this.loueur = clientLoueur;
+			this.etat = "utilisé";
+			clientLoueur.setADejaEmprunte(true);
+		} else {
+			System.out.println("erreur, voiture déjà louée");
+		}
+	}
 
-    public void retourVoiture (int combienJaiRoule, int nbJourLoc) {
-        if(this.estLoue()==true && combienJaiRoule >= 0 && nbJourLoc >=0) {
-            this.nbKm=nbKm+combienJaiRoule;
+	public void retourVoiture (int combienJaiRoule, int nbJourLoc) {
+		// if(this.estLoue() == true && combienJaiRoule >= 0 && nbJourLoc >= 0) {
+		if(this.estLoue() && combienJaiRoule >= 0 && nbJourLoc >= 0) {
+			// J'augmente le nbKm de la voiture
+			// this.nbKm = nbKm + combienJaiRoule;
+			this.nbKm += combienJaiRoule;
 
-            Facture nouvelleFacture = new Facture(this.loueur, this, this.tarifParJour*nbJourLoc);
-            this.loueur.addFacture(nouvelleFacture);
-            this.loueur=null;
+			// J'ajoute un facture au client aui a loué la voiture
+			Facture nouvelleFacture = new Facture(this.loueur, this, this.tarifParJour * nbJourLoc);
+			this.loueur.addFacture(nouvelleFacture);
 
+			// J'enleve le loueur de la voiture
+			// La voiture n'est plus loué
+			this.loueur=null;
+		} else {
+			System.out.println("erreur, voiture n'est pas louée OU tu es un couillon");
+		}
+	}
 
+	public void peindreVoiture (String newColor) {
+		this.color = newColor;
+	}
 
-        }
-        else {
-            System.out.println("erreur, voiture n'est pas louée OU tu es un couillon");
-        }
+	private boolean estLoue () {
+		/*
+		if (this.loueur != null) {
+			return true;
+		} else {
+			return false;
+		}
+		*/
 
-    }
+		// Explication
+		// Si (this.loueur != null) vaut TRUE alors Renvoyer TRUE
+		// Si (this.loueur != null) vaut FALSE alors Renvoyer FALSE
+		// Du coup on peut renvoyer this.loueur != null directement
 
-    public void peindreVoiture (String newColor) {
-        // il faut changer la couleur
-        this.color=newColor;
-
-
-    }
-
-    private boolean estLoue (){
-        if (this.loueur != null){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-
+		return this.loueur != null;
+	}
 }
