6e46ed35b5ff0455ecea03daeb4a55076033a927
Xavier-Loison <xavierloison@gmail.com>
2026-03-24 12:21:01 +0100

    - Utilisation des classes utilitaires - Ajout des this manquants - Declaration + initialisation - Getters / Setters en bas de la classe - Organisation des fonctions, affiche puis add puis autre

--- garage/src/com/doignies/marie/Client.java
+++ garage/src/com/doignies/marie/Client.java
@@ -1,6 +1,8 @@
 
 package com.doignies.marie;
 
+import com.doignies.marie.utils.Affiche;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,78 +13,72 @@ public class Client {
 	private String name;
 	private String surname;
 	private int age;
-	private List<Facture> factures;
-	private int nbEmprunt;
+	private int nbEmprunt = 0;
+	private List<Facture> factures = new ArrayList<Facture>();  // Liste vide obligatoire et non pas null (n'existe pas) car sinon la fonction addFacture() ne peut pas fonctionner. On ne peut pas ajouter dans une liste qui n'existe pas
 
 	// /////////////////////
 	// *** Constructeurs ***
 	// /////////////////////
 	public Client (final String newName, final String newSurname, final int newAge) {
-		// On ne met pas newADejaEmprunte
-		// On ne met pas newFactures
-		// dans la liste des parametres car lors de la Création d'un client
-		// 1. il n'a jamais emprunte
-		// 2. Il n'a pas encore de facture
 		this.name = newName;
 		this.surname= newSurname;
 		this.age = newAge;
-		this.factures= new ArrayList<Facture>();
-		this.nbEmprunt=0;
-		// Liste vide obligatoire et non pas null (n'existe pas)
-		// car sinon la fonction addFacture ne peut pas fonctionner
-		// On ne peut pas ajouter dans une liste qui n'existe pas
+
+		// On ne met pas newNbEmprunt / newFactures
+		// dans la liste des parametres car lors de la Création d'un client
+		// ils ont deja une valeur par defaut
+	}
+
+	// /////////////////
+	// *** Fonctions ***
+	// /////////////////
+	public void afficheToi() {
+		Affiche.a("\tClient : " + this.name + " " + this.surname + " (" + this.age + " )");
+
+		if (!this.factures.isEmpty()) { // if(factures.size() > 0) { // Protection pour ne pas afficher si il n'y a pas de factures
+			Affiche.a("\tFactures : ");
+
+			for (Facture maFacture : this.factures) {
+				Affiche.a("*");
+				maFacture.afficheToi();
+			}
+		}
+	}
+
+	public void addNbEmprunt (){
+		this.nbEmprunt ++; // this.nbEmprunt += 1;   OU   this.nbEmprunt = this.nbEmprunt + 1;
+	}
+
+	public void addFacture(Facture nouvelleFacture) {
+		this.factures.add(nouvelleFacture);
+	}
+
+	public boolean peutEmpunter () {
+		// return this.age >= 18;
+
+		if (this.age >= 18) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	// ///////////////
 	// *** Getters ***
 	// ///////////////
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
-	public int getAge() {return age;}
+	public int getNbEmprunt() {
+		return this.nbEmprunt;
+	}
 
 	public List<Facture> getFactures() {
-		return factures;
+		return this.factures;
 	}
-
-	public int getNbEmprunt() {return nbEmprunt;}
 
 	// ///////////////
 	// *** Setters ***
 	// ///////////////
-
-	// /////////////////
-	// *** Fonctions ***
-	// /////////////////
-	public void afficheToi() {
-		System.out.println ("\tClient : " + this.name + " " + this.surname);
-
-		// if(factures.size() > 0) { // Protection pour ne pas afficher si il n'y a pas de factures
-		if (!factures.isEmpty()) {
-			System.out.println("\tFactures : ");
-			for (Facture maFacture : factures) {
-				maFacture.afficheToi();
-			}
-		}
-	}
-
-	public void addFacture(Facture nouvelleFacture) {
-		factures.add(nouvelleFacture);
-	}
-
-	public boolean peutEmpunter (){
-		if(this.age>=18){
-			return true;
-		}else {
-			return false;
-			//System.out.println ("erreur, le client est mineur");
-		}
-	}
-
-	public void addNbEmprunt (){
-		this.nbEmprunt ++;
-		// this.nbEmprunt += 1;
-		// this.nbEmprunt = this.nbEmprunt + 1;
-	}
 }
